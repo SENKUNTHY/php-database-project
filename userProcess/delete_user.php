8c48b238803cfa42cb79da630dc229ee38cf55f4
Kunthy.Sen <kunthy.sen@PNCLB32-09.pnc.lan>
2021-08-23 02:43:02 +0700

    DONE OF PROJECT PHP&MYSQL SEN KUNTHY

--- userProcess/delete_user.php
+++ userProcess/delete_user.php
@@ -1,6 +1,6 @@
 <?php
     require_once('../database/database.php');
-    $user_id = $_GET['id'];
+    $user_id = $_GET['user_id'];
     $isUserDeleted = deleteUser($user_id);
     if($isUserDeleted) {
         header("Location:../index.php?page=user");
